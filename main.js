83466302564d22b2b1b1c2725f01c57a5632eed4
Millat <Millatmzere72753@gamil.com>
2024-07-24 02:18:07 +0300

    testing_1

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById("icon").addEventListener("click", function () {
+  document.getElementById("myDiv").classList.toggle("nav__open");
+});
+
+document.getElementById("middle").addEventListener("click", function () {
+  document.documentElement.classList.toggle("dark");
+});
+document.getElementById("middle2").addEventListener("click", function () {
+  document.documentElement.classList.toggle("dark");
+});
